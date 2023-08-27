3168452dd3aa81337101b3e398165a2dafb5e562
Kirill <Kovryga@rbs24.by>
2023-08-27 16:36:26 +0300

    init + db setup

---
+++ app/models/board.py
@@ -0,0 +1,40 @@
+import uuid
+
+from sqlalchemy import (
+    Column,
+    String,
+    DateTime,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from app.db import Base
+
+
+class Board(Base):
+    __tablename__ = 'boards'
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        nullable=False,
+        default=uuid.uuid4
+    )
+    name = Column(
+        String,
+        nullable=False
+    )
+    created_at = Column(
+        DateTime,
+        default=func.now()
+    )
+    updated_at = Column(
+        DateTime,
+        onupdate=func.now()
+    )
+    notes = relationship(
+        "Note",
+        back_populates="board"
+    )
+
---
+++ app/models/note.py
@@ -0,0 +1,51 @@
+import uuid
+
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    String,
+    DateTime,
+    Integer,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from app.db import Base
+
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        nullable=False,
+        default=uuid.uuid4
+    )
+    text = Column(
+        String,
+        nullable=False
+    )
+    created_at = Column(
+        DateTime,
+        default=func.now()
+    )
+    updated_at = Column(
+        DateTime,
+        onupdate=func.now()
+    )
+    views = Column(
+        Integer,
+        default=0
+    )
+
+    board_id = Column(
+        UUID,
+        ForeignKey("boards.id"),
+        ondelete="CASCADE",
+    )
+    board = relationship(
+        "Board",
+        back_populates="notes"
+    )
